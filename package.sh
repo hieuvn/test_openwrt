f0873457bfd5fca6d9927b1742e8d4fd08c63562
hieuvn <hieuphamtran@hotmail.com>
2022-06-22 11:11:15 +0700

    Update package.sh

--- package.sh
+++ package.sh
@@ -6,5 +6,5 @@ git clone --depth=1 https://github.com/koshev-msk/luci-app-smstools3
 # Add luci-app-mmconfig : configure modem cellular bands via mmcli utility
 git clone --depth=1 https://github.com/koshev-msk/luci-app-mmconfig
 
-# echo $PWD
-# mv $GITHUB_WORKSPACE/package/luci-app-ktm-tools .
+echo $PWD
+mv $GITHUB_WORKSPACE/package/luci-app-ktm-tools .
